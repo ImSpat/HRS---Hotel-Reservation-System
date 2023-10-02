6857a5018cce852d154857aaea957006f03b0952
Bartek Szczudło <bartolomaj@gmail.com>
2023-10-02 18:09:55 +0200

    Added unique ID for each guest + changed guests.html accordingly to display ID.

--- src/main/java/com/example/HRS/domain/guest/Guest.java
+++ src/main/java/com/example/HRS/domain/guest/Guest.java
@@ -3,12 +3,22 @@ package com.example.HRS.domain.guest;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 public class Guest {
 
+    private final long id;
     private final String firstName;
     private final String lastName;
     private final LocalDate birthDate;
     private final Gender gender;
+
+    public Guest(String firstName, String lastName, LocalDate birthDate, Gender gender) {
+        this.id = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
 }
