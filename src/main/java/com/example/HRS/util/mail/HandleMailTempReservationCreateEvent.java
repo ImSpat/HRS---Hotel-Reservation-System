20850b751a5907d642b70e3ae7ad1ca4c34ddcc8
Bartek Szczudło <bartolomaj@gmail.com>
2023-10-14 00:17:08 +0200

    Added sending email with confirmation link after making temp reservation

--- src/main/java/com/example/HRS/util/mail/HandleMailTempReservationCreateEvent.java
+++ src/main/java/com/example/HRS/util/mail/HandleMailTempReservationCreateEvent.java
@@ -1,13 +1,24 @@
 package com.example.HRS.util.mail;
 
 import com.example.HRS.domain.reservation.events.TempReservationCreatedEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HandleMailTempReservationCreateEvent implements ApplicationListener<TempReservationCreatedEvent> {
+
+    private EmailService emailService;
+
+    @Autowired
+    public HandleMailTempReservationCreateEvent(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @Override
     public void onApplicationEvent(TempReservationCreatedEvent event) {
         System.out.println("Mail - Handle event by implementation");
+        this.emailService.sendConfirmationEmail(event.getEmail(), event.getReservationId());
+
     }
 }
