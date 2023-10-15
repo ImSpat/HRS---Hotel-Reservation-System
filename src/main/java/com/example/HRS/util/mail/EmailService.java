304768df9dc818455cada01e2762bc3c1363de85
Bartek Szczudło <bartolomaj@gmail.com>
2023-10-15 20:56:53 +0200

    Improved how confirmation endpoint URL is constructed

--- src/main/java/com/example/HRS/util/mail/EmailService.java
+++ src/main/java/com/example/HRS/util/mail/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.HRS.util.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,14 @@ public class EmailService {
 
     private final JavaMailSender sender;
 
+    @Value("${hrs.protocol}")
+    private String protocol;
+    @Value("${hrs.domain}")
+    private String domain;
+    @Value("${hrs.port}")
+    private String port;
+    private String confirmationEndpoint = "reservations/confirm";
+
     @Autowired
     public EmailService(JavaMailSender sender) {
         this.sender = sender;
@@ -18,12 +27,12 @@ public class EmailService {
 
         SimpleMailMessage mail = new SimpleMailMessage();
 
-        String text = "http://localhost:8080/reservations/confirm"+reservationId;
+        String endpoint = String.format("%s://%s:%s/%s/%d", protocol, domain, port, confirmationEndpoint, reservationId);
 
         mail.setTo(email);
         mail.setFrom("systemhrsofficial@gmail.com");
         mail.setSubject("Potwierdź rezerwację");
-        mail.setText("Dziękujemy za dokonanie rezerwacji, by ją potwierdzić kliknij w link: " + text);
+        mail.setText("Dziękujemy za dokonanie rezerwacji, by ją potwierdzić kliknij w link: " + endpoint);
 
         this.sender.send(mail);
 
