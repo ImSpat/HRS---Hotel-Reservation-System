d06bc1435331d83a1ffa4bab0ba155cdb808468a
Bartek Szczudło <bartolomaj@gmail.com>
2023-11-02 18:19:30 +0100

    Added script with test data

---
+++ src/test/resources/create_test_data.sql
@@ -0,0 +1,12 @@
+insert into room (id, description, number, size) values (1, 'Super bardzo ładny pokój', 12, 3)
+insert into room_beds (room_id, beds) values (1, 1)
+insert into room_beds (room_id, beds) values (1, 0)
+insert into room_photos_urls (room_id, photos_urls) values (1, 'https://hips.hearstapps.com/hmg-prod/images/living-room-ideas-caroline-turner-orchardlane01-copy-1670968014.jpeg')
+
+insert into room (id, description, number, size) values (2, 'Bardzo ładny pokój', 13, 2)
+insert into room_beds (room_id, beds) values (2, 1)
+insert into room_photos_urls (room_id, photos_urls) values (2, 'https://hips.hearstapps.com/hmg-prod/images/living-room-ideas-caroline-turner-orchardlane01-copy-1670968014.jpeg')
+
+insert into room (id, description, number, size) values (3, 'Ładny pokój', 14, 1)
+insert into room_beds (room_id, beds) values (3, 0)
+insert into room_photos_urls (room_id, photos_urls) values (3, 'https://hips.hearstapps.com/hmg-prod/images/living-room-ideas-caroline-turner-orchardlane01-copy-1670968014.jpeg')
