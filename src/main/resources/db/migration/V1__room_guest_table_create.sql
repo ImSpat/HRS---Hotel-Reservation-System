cde6f6ae289a9696d145766c92eb0db21213aa04
Bartek Szczudło <bartolomaj@gmail.com>
2023-11-03 18:52:07 +0100

    Added FlyWay to project.

---
+++ src/main/resources/db/migration/V1__room_guest_table_create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE room (
+        id bigint not null auto_increment,
+        number varchar(255),
+        size integer not null,
+        description varchar(255),
+        primary key (id)
+);
+
+CREATE TABLE room_beds (room_id bigint not null, beds integer);
+
+CREATE TABLE room_photos_urls (room_id bigint not null, photos_urls varchar(255));
+
+ALTER TABLE room_beds add constraint FKmgdse5awswl23tm83cvmaerh6 foreign key (room_id) references room;
+
+ALTER TABLE room_photos_urls add constraint FK8m7edvc6gfyryjne6xluj5stv foreign key (room_id) references room;
+
+CREATE TABLE guest (
+        id bigint not null auto_increment,
+        first_name varchar(255),
+        last_name varchar(255),
+        birth_date date,
+        gender integer,
+        primary key (id)
+);
---
+++ src/main/resources/db/migration/V2__reservation_table_create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE reservation (
+        id bigint not null auto_increment,
+        from_date date,
+        to_date date,
+        confirmed boolean not null,
+        email varchar(255),
+        creation_date timestamp,
+        owner_id bigint,
+        room_id bigint,
+        primary key (id)
+);
+
+ALTER TABLE reservation add constraint FK50156niokhqlv60kjalwhi9wi foreign key (owner_id) references guest;
+
+ALTER TABLE reservation add constraint FKm8xumi0g23038cw32oiva2ymw foreign key (room_id) references room;
